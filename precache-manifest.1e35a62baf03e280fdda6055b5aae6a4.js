eadff61e82319c6b892afaad3576efd994525760
Sergii Stryzg <strizgsergey@gmail.com>
2019-12-20 22:17:40 +0200

    Updates

---
+++ precache-manifest.1e35a62baf03e280fdda6055b5aae6a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8dff98140842eee571701cd2189249d",
+    "url": "/goit-react-hw-06-phonebook/index.html"
+  },
+  {
+    "revision": "1b9e43218009a13d97e8",
+    "url": "/goit-react-hw-06-phonebook/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "673965fe7e92893e9515",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.70b6f4c0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/goit-react-hw-06-phonebook/static/js/2.70b6f4c0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b9e43218009a13d97e8",
+    "url": "/goit-react-hw-06-phonebook/static/js/main.02319c83.chunk.js"
+  },
+  {
+    "revision": "6484aee76b7c53ad04fa",
+    "url": "/goit-react-hw-06-phonebook/static/js/runtime-main.ee85b948.js"
+  }
+]);
